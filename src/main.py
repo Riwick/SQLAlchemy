b587b31e235c265af262de152c90ebb3c552d4e9
Rodion <rodbel229@gmail.com>
2024-05-06 17:18:28 +0500

    Select and update through core and orm

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-import asyncio
-import os
-import sys
-sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from src.queries.core import SyncCore
+from src.queries.orm import SyncORM
 
-from src.queries.orm import insert_data, create_tables, async_insert_data
+# SyncCore.create_tables()
 
+# SyncCore.insert_workers()
+SyncORM.select_workers()
 
-create_tables()
-
-# asyncio.run(async_insert_data())
+SyncORM.update_worker()
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,27 +1,55 @@
+from sqlalchemy import select, update
+
 from src.database import session_factory, sync_engine, async_session_factory, Base
 from src.models import WorkersORM
 
 
-def create_tables():
-    Base.metadata.drop_all(sync_engine)
-    sync_engine.echo = True
-    Base.metadata.create_all(sync_engine)
-    sync_engine.echo = True
+class SyncORM:
+
+    @staticmethod
+    def create_tables():
+        Base.metadata.drop_all(sync_engine)
+        sync_engine.echo = True
+        Base.metadata.create_all(sync_engine)
+
+    @staticmethod
+    def insert_workers():
+        with session_factory() as session:
+            worker_bobr = WorkersORM(username="Bobr")
+            worker_wolf = WorkersORM(username="Wolf")
+            session.add_all([worker_bobr, worker_wolf])
+            # session.flush()
+            session.commit()
+
+    @staticmethod
+    def select_workers():
+        with session_factory() as session:
+            query = select(WorkersORM)
+            res = session.execute(query)
+            workers = res.scalars().all()
+            print(f"{workers=}")
+
+    @staticmethod
+    def update_worker(worker_id: int = 2, new_username: str = "Michael"):
+        with session_factory() as session:
+            worker = session.get(WorkersORM, {"worker_id": worker_id})
+            worker.username = new_username
+            session.refresh(worker)
+            # stmt = (
+            #     update(WorkersORM).
+            #     values(username=new_username).
+            #     filter(WorkersORM.worker_id == worker_id)
+            # )
+            # session.execute(stmt)
+            session.commit()
 
 
-def insert_data():
-    with session_factory() as session:
-        worker_bobr = WorkersORM(username="Bobr")
-        worker_wolf = WorkersORM(username="Wolf")
-        session.add_all([worker_bobr, worker_wolf])
-        session.commit()
+class AsyncORM:
 
-
-async def async_insert_data():
-    async with async_session_factory() as session:
-        worker_bobr = WorkersORM(username="Bobr")
-        worker_wolf = WorkersORM(username="Wolf")
-        session.add_all([worker_bobr, worker_wolf])
-        await session.commit()
-
-
+    @staticmethod
+    async def async_insert_data():
+        async with async_session_factory() as session:
+            worker_bobr = WorkersORM(username="Bobr")
+            worker_wolf = WorkersORM(username="Wolf")
+            session.add_all([worker_bobr, worker_wolf])
+            await session.commit()
